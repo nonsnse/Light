c5e5b457c295bb0ed9d3901f76cdf2477aa17af0
Amplify Network <not.amplify.x@gmail.com>
2024-05-06 21:22:50 +0000

    fixed ads toggles

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -227,14 +227,23 @@ if (localStorage.getItem("backgroundImage")) {
 applyCloakSettings();
 
 var adsOn = window.localStorage.getItem("adsOn");
+var gAdsOn = window.localStorage.getItem("gAdsOn");
+
 if (adsOn === "false") {
   console.log("User said no ads :( okkkk");
 } else {
   const gascript = document.createElement("script");
   gascript.setAttribute("src", "//pl23207130.highcpmgate.com/7f/1d/1c/7f1d1c315887fde89dd6ce89a57b9d57.js");
   document.head.append(gascript);
-  const gascript2 = document.createElement("script");
-  gascript2.setAttribute("src", "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2804638430420139");
-  document.head.append(gascript2);
   console.log("Added Advert Script");
+}
+
+if (gAdsOn === "false") {
+  console.log("User said no google ads :( okkkk");
+} else {
+  const googleascript = document.createElement("script");
+  googleascript.setAttribute("src", "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2804638430420139");
+  googleascript.setAttribute("crossorigin", "anonymous");
+  document.head.append(googleascript);
+  console.log("Added Goggle Adsense Script");
 }
